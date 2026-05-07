937ae0708fb826bad21db526758b1e67797c61e7
sellasouls666 <pgbc303@gmail.com>
2026-05-07 15:03:24 +0300

    сделал примерный внешний вид страницы конфигуратора

---
+++ DDMdiplom/Controllers/PcBuilderController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DDMdiplom.Controllers
+{
+    public class PcBuilderController : Controller
+    {
+        [Route("PcBuilder")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
