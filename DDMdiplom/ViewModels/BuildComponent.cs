e350d114a77cde536ce69d7aac79ae74849be292
sellasouls666 <pgbc303@gmail.com>
2026-05-13 18:12:09 +0300

    разработка конфигуратора на странице процессоров

--- DDMdiplom/ViewModels/BuildComponent.cs
+++ DDMdiplom/ViewModels/BuildComponent.cs
@@ -1,10 +1,9 @@
-﻿namespace DDMdiplom.ViewModels
+﻿public class BuildComponent
 {
-    public class BuildComponent
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-        public decimal Price { get; set; }
-        public string Type { get; set; } = string.Empty; // "Процессор", "Видеокарта" и т.д.
-    }
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+    public string Type { get; set; } = string.Empty;
+    public string? ImageUrl { get; set; }
+    public int? Tdp { get; set; } // для процессора, видеокарты и т.д.
 }
