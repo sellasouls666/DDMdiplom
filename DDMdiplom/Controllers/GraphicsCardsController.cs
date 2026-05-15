5a248c2da317d3dc1e3b952585be3695d9b85e39
sellasouls666 <pgbc303@gmail.com>
2026-05-15 17:03:10 +0300

    сделал страницу с видеокартами

---
+++ DDMdiplom/Controllers/GraphicsCardsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DDMdiplom.Data;
+using DDMdiplom.Models;
+
+namespace DDMdiplom.Controllers
+{
+    public class GraphicsCardsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GraphicsCardsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var cards = await _context.GraphicsCards.ToListAsync();
+            return View(cards);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetGraphicsCard(int id)
+        {
+            var card = await _context.GraphicsCards.FindAsync(id);
+            if (card == null) return NotFound();
+            return Ok(card);
+        }
+    }
+}
