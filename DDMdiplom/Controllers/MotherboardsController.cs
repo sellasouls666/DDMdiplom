ba0d146f7fbed3cadd679de4a7a3d42154f49548
sellasouls666 <pgbc303@gmail.com>
2026-05-17 16:47:35 +0300

    сделал страницу с материнскими платами и переходы на неё

---
+++ DDMdiplom/Controllers/MotherboardsController.cs
@@ -0,0 +1,31 @@
+﻿using DDMdiplom.Data;
+using DDMdiplom.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DDMdiplom.Controllers
+{
+    public class MotherboardsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MotherboardsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var motherboards = await _context.Motherboards.ToListAsync();
+            return View(motherboards);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetMotherboard(int id)
+        {
+            var motherboard = await _context.Motherboards.FindAsync(id);
+            if (motherboard == null) return NotFound();
+            return Ok(motherboard);
+        }
+    }
+}
