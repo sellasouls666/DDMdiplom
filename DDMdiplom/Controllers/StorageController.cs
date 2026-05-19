135b15690ed62b4f1a8b1fc656b461c38d722cfe
sellasouls666 <pgbc303@gmail.com>
2026-05-19 16:09:08 +0300

    сделал страницу с выбором типа накопителя

---
+++ DDMdiplom/Controllers/StorageController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DDMdiplom.Controllers
+{
+    public class StorageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult HardDrives()
+        {
+            // Пока просто возвращаем пустую страницу, потом заменим на список HDD
+            return View();
+        }
+
+        public IActionResult Ssds()
+        {
+            // Пока просто возвращаем пустую страницу, потом заменим на список SSD
+            return View();
+        }
+    }
+}
