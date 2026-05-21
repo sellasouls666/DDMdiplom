4d4781cac5d812b9da0d005e7ff93daf688164f3
sellasouls666 <pgbc303@gmail.com>
2026-05-21 17:36:03 +0300

    сделал страницу с операционными системами

--- DDMdiplom/Controllers/OperatingSystemsController.cs
+++ DDMdiplom/Controllers/OperatingSystemsController.cs
@@ -1,12 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DDMdiplom.Data;
+using DDMdiplom.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DDMdiplom.Controllers
 {
     public class OperatingSystemsController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public OperatingSystemsController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        // GET: /OperatingSystems
+        public async Task<IActionResult> Index()
+        {
+            var osList = await _context.OperatingSystems.ToListAsync();
+            return View(osList);
+        }
+
+        // GET: /OperatingSystems/GetOperatingSystem/{id}
+        [HttpGet]
+        public async Task<IActionResult> GetOperatingSystem(int id)
+        {
+            var os = await _context.OperatingSystems.FindAsync(id);
+            if (os == null) return NotFound();
+            return Json(os);
         }
     }
 }
