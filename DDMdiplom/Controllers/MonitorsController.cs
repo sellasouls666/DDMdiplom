f62289a2a4ebb9480c28e64692bfa094a1c8ded9
sellasouls666 <pgbc303@gmail.com>
2026-05-21 17:41:21 +0300

    сделал страницу с выбором типа монитора

---
+++ DDMdiplom/Controllers/MonitorsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DDMdiplom.Controllers
+{
+    public class MonitorsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GamingMonitors()
+        {
+            return View();
+        }
+
+        public IActionResult LcdMonitors()
+        {
+            return View();
+        }
+    }
+}
