1b28163b62578908527d77b787f6d3472e7f92f5
sellasouls666 <pgbc303@gmail.com>
2026-05-21 13:45:36 +0300

    сделал страницу с выбором вида системы охлаждения

---
+++ DDMdiplom/Controllers/CoolingController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DDMdiplom.Controllers
+{
+    public class CoolingController : Controller
+    {
+        // GET: /Cooling – выбор типа охлаждения
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /Cooling/AirCoolers – воздушные кулеры (Fan & Heatsink)
+        public IActionResult AirCoolers()
+        {
+            // В будущем замените на получение данных из БД
+            return View();
+        }
+
+        // GET: /Cooling/LiquidCoolers – водяные системы (Water Cooler Kit)
+        public IActionResult LiquidCoolers()
+        {
+            // В будущем замените на получение данных из БД
+            return View();
+        }
+    }
+}
