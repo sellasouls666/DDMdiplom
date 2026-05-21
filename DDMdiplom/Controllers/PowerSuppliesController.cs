c7f1966cfc546b14bf571863f0cef14aa93f3bab
sellasouls666 <pgbc303@gmail.com>
2026-05-21 13:17:41 +0300

    сделал страницу с блоками питания

---
+++ DDMdiplom/Controllers/PowerSuppliesController.cs
@@ -0,0 +1,34 @@
+﻿using DDMdiplom.Data;
+using DDMdiplom.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DDMdiplom.Controllers
+{
+    public class PowerSuppliesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PowerSuppliesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: /PowerSupplies
+        public async Task<IActionResult> Index()
+        {
+            var powerSupplies = await _context.PowerSupplies.ToListAsync();
+            return View(powerSupplies);
+        }
+
+        // GET: /PowerSupplies/GetPowerSupply/{id}
+        [HttpGet]
+        public async Task<IActionResult> GetPowerSupply(int id)
+        {
+            var psu = await _context.PowerSupplies.FindAsync(id);
+            if (psu == null)
+                return NotFound();
+            return Json(psu);
+        }
+    }
+}
