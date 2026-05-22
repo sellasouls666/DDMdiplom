b50a3fefd7b8eb408b3707af6473284407a1319c
sellasouls666 <pgbc303@gmail.com>
2026-05-22 17:02:34 +0300

    сделал страницу с выбором типа мыши

---
+++ DDMdiplom/Controllers/MiceController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DDMdiplom.Controllers
+{
+    public class MiceController : Controller
+    {
+        // GET: /Mice – выбор типа мыши
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /Mice/GamingMice – игровые мыши
+        public IActionResult GamingMice()
+        {
+            // Временно без БД – позже можно загрузить из контекста
+            return View();
+        }
+
+        // GET: /Mice/Mice – обычные мыши
+        public IActionResult Mice()
+        {
+            return View();
+        }
+    }
+}
