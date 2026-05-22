8d90363bfb90e3d6be8cc92644913f46968daa8a
sellasouls666 <pgbc303@gmail.com>
2026-05-22 16:37:50 +0300

    сделал страницу с выбором типа клавиатуры

---
+++ DDMdiplom/Controllers/KeyboardsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DDMdiplom.Controllers
+{
+    public class KeyboardsController : Controller
+    {
+        // GET: /Keyboards – выбор типа клавиатуры
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /Keyboards/GamingKeyboards – игровые клавиатуры
+        public IActionResult GamingKeyboards()
+        {
+            // Временно без БД
+            return View();
+        }
+
+        // GET: /Keyboards/Keyboards – обычные клавиатуры
+        public IActionResult Keyboards()
+        {
+            return View();
+        }
+    }
+}
