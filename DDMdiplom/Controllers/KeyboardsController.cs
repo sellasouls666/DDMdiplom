4e489fc6d4edbf93725b778a695d82e7cd41d9e0
sellasouls666 <pgbc303@gmail.com>
2026-05-22 16:42:52 +0300

    сделал страницу с игровыми клавиатурами

--- DDMdiplom/Controllers/KeyboardsController.cs
+++ DDMdiplom/Controllers/KeyboardsController.cs
@@ -1,9 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DDMdiplom.Data;
+using DDMdiplom.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DDMdiplom.Controllers
 {
     public class KeyboardsController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public KeyboardsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         // GET: /Keyboards – выбор типа клавиатуры
         public IActionResult Index()
         {
@@ -11,16 +21,32 @@ namespace DDMdiplom.Controllers
         }
 
         // GET: /Keyboards/GamingKeyboards – игровые клавиатуры
-        public IActionResult GamingKeyboards()
+        public async Task<IActionResult> GamingKeyboards()
         {
-            // Временно без БД
-            return View();
+            var keyboards = await _context.Keyboards
+                .Where(k => k.Type != null &&
+                            (k.Type.Contains("Gaming") || k.Type.Contains("Игровая")))
+                .ToListAsync();
+            return View(keyboards);
         }
 
         // GET: /Keyboards/Keyboards – обычные клавиатуры
-        public IActionResult Keyboards()
+        public async Task<IActionResult> Keyboards()
         {
-            return View();
+            var keyboards = await _context.Keyboards
+                .Where(k => k.Type == null ||
+                            (!k.Type.Contains("Gaming") && !k.Type.Contains("Игровая")))
+                .ToListAsync();
+            return View(keyboards);
+        }
+
+        // GET: /Keyboards/GetKeyboard/{id} – JSON для модального окна
+        [HttpGet]
+        public async Task<IActionResult> GetKeyboard(int id)
+        {
+            var keyboard = await _context.Keyboards.FindAsync(id);
+            if (keyboard == null) return NotFound();
+            return Json(keyboard);
         }
     }
 }
