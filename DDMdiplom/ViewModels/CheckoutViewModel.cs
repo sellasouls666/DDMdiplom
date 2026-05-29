93c5a392a15bd8ae3d74c94a5cecfd4f43d13236
sellasouls666 <pgbc303@gmail.com>
2026-05-29 16:23:55 +0300

    добавил валидацию заказам

--- DDMdiplom/ViewModels/CheckoutViewModel.cs
+++ DDMdiplom/ViewModels/CheckoutViewModel.cs
@@ -2,20 +2,27 @@
 
 namespace DDMdiplom.ViewModels
 {
-    // ViewModels/CheckoutViewModel.cs
     public class CheckoutViewModel
     {
-        public string? CustomerName { get; set; }
-        public string? Phone { get; set; }
-        public string? Address { get; set; }
+        [Required(ErrorMessage = "Укажите полное имя")]
+        public string CustomerName { get; set; }
+
+        [Required(ErrorMessage = "Укажите номер телефона")]
+        [Phone(ErrorMessage = "Введите корректный номер")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Введите адрес доставки")]
+        public string Address { get; set; }
+
         public string? Comment { get; set; }
+
         public List<CartItemSummaryViewModel> CartItems { get; set; } = new();
         public decimal TotalPrice { get; set; }
     }
 
     public class CartItemSummaryViewModel
     {
-        public int BuildId { get; set; }          // <-- добавили
+        public int BuildId { get; set; }
         public string BuildName { get; set; } = string.Empty;
         public int Quantity { get; set; }
         public decimal Price { get; set; }
